3dd2b84c47cf60b582e0f9dd17ae0795eb84a12e
amarjit <datta.amarjit@gmail.com>
2017-10-12 23:39:21 -0500

    Changed the server code. The server now can act both as server and as client.

--- src/com/dagos/impl/Server.java
+++ src/com/dagos/impl/Server.java
@@ -4,19 +4,19 @@ import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 import com.dagos.interfaces.Hello;
 
 public class Server implements Hello {
-	public Server() {		
-	}
-	
-	public String sayHello() {
+    public Server() {
+    }
+
+    public String sayHello() {
         return "Hello, world!";
     }
 
-    public static void main(String args[]) {
-        System.setProperty("java.rmi.server.hostname", "172.16.45.128");
+    private static void InitializeServer() {
         try {
             Server obj = new Server();
             Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
@@ -30,5 +30,29 @@ public class Server implements Hello {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
         }
+    }
+
+    private static void InitializeClient(String[] args) {
+        String host = (args.length < 1) ? null : args[0];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Send request to remote server?");
+        System.out.println("Enter anything to continue and 'no' to exit");
+        while (!scanner.nextLine().equalsIgnoreCase("no")) {
+            try {
+                Registry registry = LocateRegistry.getRegistry(host);
+                Hello stub = (Hello) registry.lookup("Hello");
+                String response = stub.sayHello();
+                System.out.println("response from " + host + ": " + response);
+            } catch (Exception e) {
+                System.err.println("Client exception: " + e.toString());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        System.setProperty("java.rmi.server.hostname", "172.16.45.128");
+        InitializeServer();
+        InitializeClient(args);
     }
 }
